8eeb56d520dca34f6b0f8b18d76abc2bc0c73062
Joseph S. Lipinski <jsl94@drexel.edu>
2018-08-26 18:23:10 -0400

    Basic UI

---
+++ Assets/Scripts/Menu_Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Buttons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame(){
+        //SceneManager.LoadScene("MainScene");
+    }
+
+    public void ViewControls(){
+        //SceneManager.LoadScene("ControlsScreen");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
